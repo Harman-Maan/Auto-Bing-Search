f3e338b19756fd2710ae173df5bf11e11aa927bd
maansaab95170 <maansaab95170@gmail.com>
2023-06-17 00:45:57 +0530

    Update

--- js/reward-videos.js
+++ js/reward-videos.js
@@ -1,4 +1,4 @@
-import data from "./assets/Fitness-video-data.js";
+import data from "../assets/Fitness-video-data.js";
 
 const playVideoBtn = document.getElementById("play-video-btn");
 const videoPlayingSection = document.getElementById("video-playing");
@@ -6,14 +6,14 @@ const playVideoContent = document.getElementById("play-video-content");
 const stopVideo = document.getElementById("stop-video");
 const estimatedVideoLeft = document.getElementById("estimated-video-left");
 const videoDuration = document.getElementById("video-duration");
-const vidoesWatched = document.getElementById("videos-watched");
+const videosWatched = document.getElementById("videos-watched");
 let currentId;
 
 playVideoBtn.addEventListener("click", createVideoTab);
 stopVideo.addEventListener("click", stopVideoTimeOut);
 
-sessionStorage.setItem("videosPlayed", 0);
-let videosPlayed = sessionStorage.getItem("videosPlayed");
+let date = `${new Date().getFullYear()}/${new Date().getMonth()}/${new Date().getDate()}`;
+let videosPlayed;
 let count = 0;
 
 let setCounter;
@@ -23,25 +23,41 @@ function checkLocalStorage() {
   if (localStorage.hasOwnProperty("currentId")) {
     currentId = localStorage.getItem("currentId");
     videoDuration.textContent = data[currentId].duration;
+    videosPlayed = localStorage.getItem("videosPlayed");
+
+    let oldDate = localStorage.getItem("date");
+    if (date != oldDate) {
+      localStorage.setItem("date", date);
+      localStorage.setItem("videosPlayed", 0);
+    } else if (videosPlayed >= 2) {
+      reachedLimitWarning();
+    }
   } else {
     localStorage.setItem("currentId", 0);
+    localStorage.setItem("videosPlayed", 0);
+    localStorage.setItem("date", date);
     checkLocalStorage();
   }
 }
 checkLocalStorage();
 
 function createVideoTab() {
+  function counter() {
+    estimatedVideoLeft.textContent = count;
+    count--;
+  }
+
   displayVideoPlayingSection();
 
   let { duration, url } = data[currentId];
   videoDuration.textContent = duration;
 
   window.open(`https://www.msn.com/en-us/health/fitnessworkoutvideo/${url}`, "_blank");
-  // console.log(currentId); // -------------------------------------
+  // console.log(currentId);
 
+  count = duration + 5;
   setCounter = setInterval(counter, 1000);
   setVideoTimeOut = setTimeout(videoEnded, (duration + 5) * 1000);
-  count = duration + 5;
 }
 
 function displayVideoPlayingSection() {
@@ -54,20 +70,21 @@ function hideVideoPlayingSection() {
 }
 
 function reachedLimitWarning() {
-  vidoesWatched.parentElement.textContent = `You have reached the limit of videos you can watch each day.`;
+  videosWatched.parentElement.textContent = `You have reached the limit of videos you can watch each day.`;
   playVideoBtn.textContent = "Play Anyway";
 }
 
 function videoEnded() {
-  sessionStorage.setItem("videosPlayed", videosPlayed++);
-  vidoesWatched.textContent = videosPlayed;
-  if (videosPlayed == 2) reachedLimitWarning();
+  videosPlayed++;
+  localStorage.setItem("videosPlayed", videosPlayed);
+  if (videosPlayed >= 2) reachedLimitWarning();
+  else videosWatched.textContent = videosPlayed;
 
   currentId++;
   if (currentId >= data.length) currentId = 0;
-  console.log(currentId);
 
   localStorage.setItem("currentId", currentId);
+  videoDuration.textContent = data[currentId].duration;
   stopVideoTimeOut();
 }
 
@@ -77,9 +94,4 @@ function stopVideoTimeOut() {
   clearTimeout(setVideoTimeOut);
   hideVideoPlayingSection();
   estimatedVideoLeft.textContent = "_-_";
-}
-
-function counter() {
-  estimatedVideoLeft.textContent = count;
-  count--;
 }
